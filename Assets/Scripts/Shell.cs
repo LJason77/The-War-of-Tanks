f285017b2e1651b44420f50943d1b20d33353b08
LJason <ljason@ljason77.com>
2023-09-06 23:17:35 +0800

    添加爆炸冲力和范围
    Signed-off-by: LJason <ljason@ljason77.com>

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -1,20 +1,29 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Shell : MonoBehaviour
 {
-    [Tooltip("爆炸特效")]
-    public GameObject explosionEffect;
-    [Tooltip("爆炸特效销毁的延迟时间")]
-    public float explosionTimeUp = 1.5f;
+    [Tooltip("爆炸冲力")] public float explosionForce = 300f;
+    [Tooltip("爆炸范围")] public float explosionRadius = 3f;
+    [Tooltip("爆炸特效")] public GameObject explosionEffect;
+    [Tooltip("爆炸特效销毁的延迟时间")] public float explosionTimeUp = 1.5f;
 
-    void OnCollisionEnter()
+    private void OnCollisionEnter()
     {
-        var tf = transform;
-        GameObject effect = Instantiate(explosionEffect, tf.position, tf.rotation);
+        Transform tf = transform;
+        Vector3 position = tf.position;
+        GameObject effect = Instantiate(explosionEffect, position, tf.rotation);
         Destroy(gameObject);
         Destroy(effect, explosionTimeUp);
+
+        Collider[] colliders = Physics.OverlapSphere(position, explosionRadius);
+        if (colliders.Length <= 0) return;
+        foreach (Collider col in colliders)
+        {
+            #if UNITY_EDITOR
+            Debug.Log(col.gameObject.name);
+            #endif
+            Rigidbody rb = col.GetComponent<Rigidbody>();
+            if (rb != null) rb.AddExplosionForce(explosionForce, position, explosionRadius);
+        }
     }
 }

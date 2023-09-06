71a1edaf7c75437b52d1d446ebcb2dc74f4ac62d
LJason <ljason@ljason77.com>
2023-09-06 20:28:41 +0800

    优化坦克控制脚本
    Signed-off-by: LJason <ljason@ljason77.com>

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Tank : MonoBehaviour
@@ -10,24 +8,15 @@ public class Tank : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector3.forward * (moveSpeed * Time.deltaTime));
-        }
+        var vertical = Input.GetAxis("Vertical1");
+        var horizontal = Input.GetAxis("Horizontal1");
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.back * (moveSpeed * Time.deltaTime));
-        }
+        transform.Translate(Vector3.forward * (moveSpeed * Time.deltaTime * vertical));
+        transform.Rotate(Vector3.up * (rotateSpeed * Time.deltaTime * horizontal));
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.down * (rotateSpeed * Time.deltaTime));
-        }
-        
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(Vector3.up * (rotateSpeed * Time.deltaTime));
-        }
+        // if (Input.GetKey(KeyCode.W)) transform.Translate(Vector3.forward * (moveSpeed * Time.deltaTime));
+        // if (Input.GetKey(KeyCode.S)) transform.Translate(Vector3.back * (moveSpeed * Time.deltaTime));
+        // if (Input.GetKey(KeyCode.A)) transform.Rotate(Vector3.down * (rotateSpeed * Time.deltaTime));
+        // if (Input.GetKey(KeyCode.D)) transform.Rotate(Vector3.up * (rotateSpeed * Time.deltaTime));
     }
 }

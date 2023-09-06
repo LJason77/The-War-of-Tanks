071b3241010e08a1ba22f3c1348e967f6b3c95f5
LJason <ljason@ljason77.com>
2023-09-06 20:07:42 +0800

    延迟销毁炮弹特效
    Signed-off-by: LJason <ljason@ljason77.com>

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 
 public class Shell : MonoBehaviour
 {
+    [Tooltip("爆炸特效")]
     public GameObject explosionEffect;
+    [Tooltip("爆炸特效销毁的延迟时间")]
+    public float explosionTimeUp = 1.5f;
 
     void OnCollisionEnter()
     {
         var tf = transform;
-        Instantiate(explosionEffect, tf.position, tf.rotation);
+        GameObject effect = Instantiate(explosionEffect, tf.position, tf.rotation);
         Destroy(gameObject);
+        Destroy(effect, explosionTimeUp);
     }
 }
